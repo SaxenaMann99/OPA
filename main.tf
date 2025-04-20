134ec7d681f01f94a390c0b4931170f82ac8b973
SaxenaMann99 <166237850+SaxenaMann99@users.noreply.github.com>
2025-04-20 14:47:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(/tmp/credentials.json)
+  credentials = file("/tmp/credentials.json")
   project     = "project-terraform-457305"
   region      = "asia-south2"
 }
@@ -13,16 +13,16 @@ resource "google_storage_bucket" "example_bucket" {
 }
 
 # Optional: Define bucket ACLs
-#resource "google_storage_bucket_acl" "example_bucket_acl" {
-#  bucket = google_storage_bucket.example_bucket.name
-#  role_entity = [
-#    "READER:allUsers", # This will trigger the OPA policy
-#  ]
-#}
+resource "google_storage_bucket_acl" "example_bucket_acl" {
+  bucket = google_storage_bucket.example_bucket.name
+  role_entity = [
+    "READER:allUsers", # This will trigger the OPA policy
+  ]
+}
 
 # Optional: Define bucket access control
-#resource "google_storage_bucket_access_control" "example_bucket_access_control" {
-#  bucket = google_storage_bucket.example_bucket.name
-#  entity = "allUsers"
-#  role   = "READER" # This will trigger the OPA policy
-#}
+resource "google_storage_bucket_access_control" "example_bucket_access_control" {
+  bucket = google_storage_bucket.example_bucket.name
+  entity = "allUsers"
+  role   = "READER" # This will trigger the OPA policy
+}

9471bd5f81befb2a310f445af680d671d712cf32
SaxenaMann99 <166237850+SaxenaMann99@users.noreply.github.com>
2025-04-20 14:37:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  credentials = file(/tmp/credentials.json)
+  project     = "project-terraform-457305"
+  region      = "asia-south2"
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name     = "opa-test"
+  location = "Asia"
+
+  Uncomment to test the OPA policy
+  acl = "publicRead"
+}
+
+# Optional: Define bucket ACLs
+#resource "google_storage_bucket_acl" "example_bucket_acl" {
+#  bucket = google_storage_bucket.example_bucket.name
+#  role_entity = [
+#    "READER:allUsers", # This will trigger the OPA policy
+#  ]
+#}
+
+# Optional: Define bucket access control
+#resource "google_storage_bucket_access_control" "example_bucket_access_control" {
+#  bucket = google_storage_bucket.example_bucket.name
+#  entity = "allUsers"
+#  role   = "READER" # This will trigger the OPA policy
+#}

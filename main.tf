61e489781570318f63ca6c9b27a58c03acaaeb9a
SaxenaMann99 <166237850+SaxenaMann99@users.noreply.github.com>
2025-04-20 20:09:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_storage_bucket" "example_bucket" {
   name     = "opa-test"
   location = "Asia"
-  public_access_prevention = "enforced"
+#  public_access_prevention = "enforced"
 }
 
 # Optional: Define bucket ACLs
